3a2b02bddc8efdcfd79e0dae2f24e4d27067036e
Aritra <ari@aritra.xyz>
2023-01-04 20:47:23 +0530

    add bad-words filter

--- requests_responses/controllers/blogController.js
+++ requests_responses/controllers/blogController.js
@@ -1,49 +1,58 @@
-const Blog = require('../models/blog')
+const Filter = require("bad-words")
+const Blog = require("../models/blog")
 
 const blog_index = (req, res) => {
-    Blog.find().sort({createdAt: -1})
-        .then((result)=> {
-            res.render('blogs/index', { title: "all blogs", blogs: result })
-        })
-        .catch((err)=>console.log(err))
+  Blog.find()
+    .sort({ createdAt: -1 })
+    .then((result) => {
+      res.render("blogs/index", { title: "all blogs", blogs: result })
+    })
+    .catch((err) => console.log(err))
 }
 
 const blog_details = (req, res) => {
-    const id = req.params.id
-    Blog.findById(id)
-        .then(result => {
-            res.render('blogs/details', { title: id, blog: result})
-        })
-        .catch(err => res.status(404).render('404', {title: 'Blog Not Found'}))
+  const id = req.params.id
+  Blog.findById(id)
+    .then((result) => {
+      res.render("blogs/details", { title: id, blog: result })
+    })
+    .catch((err) => res.status(404).render("404", { title: "Blog Not Found" }))
 }
 
 const blog_create_post = (req, res) => {
-    const blog = new Blog(req.body)
-    blog.save()
-        .then(result => {
-            res.redirect('/blogs')
-        })
-        .catch(err => console.log(err))
+  const filter = new Filter()
+
+  for (const key in req.body) {
+    req.body[key] = filter.clean(req.body[key])
+  }
+  //   console.log(req.body)
+  const blog = new Blog(req.body)
+
+  blog
+    .save()
+    .then((result) => {
+      res.redirect("/blogs")
+    })
+    .catch((err) => console.log(err))
 }
 
 const blog_create_get = (req, res) => {
-    res.render('blogs/create', {title: 'Create'})
+  res.render("blogs/create", { title: "Create" })
 }
 
-
 const blog_delete = (req, res) => {
-    const id = req.params.id
-    Blog.findByIdAndDelete(id)
-        .then(result => {
-            res.json({ redirect: "/blogs" })
-        })
-        .catch(err => console.log(err))
+  const id = req.params.id
+  Blog.findByIdAndDelete(id)
+    .then((result) => {
+      res.json({ redirect: "/blogs" })
+    })
+    .catch((err) => console.log(err))
 }
 
 module.exports = {
-    blog_index,
-    blog_details,
-    blog_create_get,
-    blog_create_post,
-    blog_delete
+  blog_index,
+  blog_details,
+  blog_create_get,
+  blog_create_post,
+  blog_delete,
 }
